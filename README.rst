79cfac0ff0fc9ed8f1f7b1c1296b204e7d672e82
Martin Schobert <martin@schobert.cc>
2019-08-09 15:23:22 +0200

    initial import

---
+++ README.rst
@@ -0,0 +1,23 @@
+``simple_fax_sms`` is a python script for sending SMS via simple-fax.de.
+
+
+Installation
+==================
+
+git install git+https://github.com/nitram2342/simple-fax-sms
+
+
+Usage example
+===============
+
+The ``fensterbrief`` tool is command line based:
+
+::
+   SIMPLEFAXDE_PASS=mysecret simple_fax_sms --phone +49xxxxxx --user login@example.com --text 'Hello, World"
+
+Copyright and Licence
+=====================
+
+``fensterbrief`` is developed by Martin Schobert martin@weltregierung.de and
+published under a BSD licence with a non-military clause. Please read
+``LICENSE.txt`` for further details.
---
+++ simple_fax_sms/__init__.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2019 Martin Schobert <martin@weltregierung.de>
+#  
+
+""" The main routine for the sms script """
+
+import argparse
+import requests
+import os
+
+ENV_VAR = "SIMPLEFAXDE_PASS"
+
+def login(user, password):
+    session = requests.Session()
+
+    r = session.post("https://simple-fax.de/app/", data={'username' : user,
+                                                         'password' : password,
+                                                         'option' : 'com_users',
+                                                         'task' : 'user.login'}, allow_redirects=False)
+    
+    if "11" in r.text:
+        print("+ Logged into service.")
+        return session
+    elif "error" in r.text:
+        print("+ Error detected. Invalid credentials?")
+        return None
+    else:
+        print("+ Unknown error.")
+        return None
+
+def send_sms(session, phone, text):
+    if session:
+        r = session.post("https://simple-fax.de/app/versandbox?task=neuesms",
+                         data={'handynummern' : phone,
+                               'sms_content' : text,
+                               'task' : 'smssenden'})
+        
+        if r.status_code == 200:
+            # It does not make much sense to do further checks:
+            # - Code 200 is always returned, but does not mean anything beyond the PHP page is running.
+            # - "Ihr SMS ist auf dem Weg!" is always printed, even if you forget the phone number.
+            # - An inline image shows a green or red sign, but is not really parsable.
+            return True
+
+    return False
+    
+def main():
+
+    # process command line arguments
+    parser = argparse.ArgumentParser(description='Manage letters via command line', add_help=False)
+
+    parser.add_argument('-h', '--help', action='store_true', dest='help')
+    parser.add_argument('--user', help=f'The simple-fax.de user name. The password is expected to be stored in the environment variable {ENV_VAR}', metavar="MAILADDRESS")
+    parser.add_argument('--phone', help='The destination phone number.', metavar="MAILADDRESS")
+    parser.add_argument('--text', help='The SMS text to send.', metavar="TEXT")
+
+    (options, unknown_options) = parser.parse_known_args()
+        
+                      
+    if unknown_options:
+        print("+ Unknown options: %s" % unknown_options)
+        parser.print_help()
+        return
+
+    if options.help:
+        parser.print_help()
+        return
+        
+    if options.phone and options.text and options.user:
+        password = os.environ[ENV_VAR]
+        if password is None:
+            print(f"+ Failed to get password from envionment variable {ENV_VAR}")
+            return
+        else:
+            session = login(options.user,  password)
+            if session:
+                send_sms(session, options.phone, options.text)
+    else:
+        parser.print_help()
+        return
+        
+    
+if __name__ == "__main__":
+    main()
+
